0cebf30ae38772f2dcdca0d524939c0e41ee96a6
Saba <siddiquisaba.2003@gmail.com>
2023-06-06 17:03:20 -0400

    imported app.scss into app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import './App.scss';
 function App() {
   return (
     <BrowserRouter>
