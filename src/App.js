9e2ca5e5fdca3d4b5e0fd63dd5b138c5c2ca7b7a
Aia_makhruka <aia.makhruka@gmail.com>
2023-06-06 15:26:35 -0400

    modify the errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import './App.js'
 function App() {
   return (
     <BrowserRouter>
