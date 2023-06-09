624add831604b4405f615321a210e04adbb2e964
Aia_makhruka <aia.makhruka@gmail.com>
2023-06-09 18:17:34 -0400

    removing comments

--- src/components/InventoryPageList/InventoryPageList.js
+++ src/components/InventoryPageList/InventoryPageList.js
@@ -36,72 +36,6 @@ function InventoryPageList() {
   console.log(InventoryList);
   return (
     <>
-    
-      {/* <section className="inventories">
-        <div className="inventories__header">
-          <h1 className="inventories__header-title">inventory</h1>
-          <div className="inventories__header-container">
-            <form className="inventories__header-form">
-              <input
-                type="search"
-                name="search"
-                className="inventories__header-form-search"
-                placeholder="Search..."
-              />
-              <Link to={"#"}>
-                <button className="inventories__header-form-button">
-                  + Add New Warehouse
-                </button>
-              </Link>
-            </form>
-          </div>
-        </div>
-
-        <section className="inventories__heading">
-          <div className="inventories__container inventories__container--inventoryitemWidth">
-            <h4 className="inventories__heading-warehouse">INVENTORYITEM</h4>
-            <img
-              src={Arrows}
-              alt="filter-arrows"
-              className="inventories__heading-icon "
-            />
-          </div>
-          <div className="inventories__container inventories__container--categoryWidth">
-            <h4 className="inventories__heading-warehouse">CATEGORY</h4>
-            <img
-              src={Arrows}
-              alt="filter-arrows"
-              className="inventories__heading-icon"
-            />
-          </div>
-          <div className="inventories__container inventories__container--statusWitdh">
-            <h4 className="inventories__heading-warehouse">STATUS</h4>
-            <img
-              src={Arrows}
-              alt="filter-arrows"
-              className="inventories__heading-icon"
-            />
-          </div>
-          <div className="inventories__container inventories__container--qtyWitdh">
-            <h4 className="inventories__heading-warehouse">QTY</h4>
-            <img
-              src={Arrows}
-              alt="filter-arrows"
-              className="inventories__heading-icon"
-            />
-          </div>
-          <div className="inventories__container inventories__container--warehousenameWidth">
-            <h4 className="inventories__heading-warehouse">WAREHOUSE</h4>
-            <img
-              src={Arrows}
-              alt="filter-arrows"
-              className="inventories__heading-icon"
-            />
-          </div>
-          <div className="inventories__container inventories__container--actionsWidth">
-            <h4 className="inventories__heading-warehouse">ACTIONS</h4>
-          </div>
-        </section> */}
         {InventoryList.map((itemDetails) => {
           console.log(itemDetails);
           return (
@@ -112,7 +46,6 @@ function InventoryPageList() {
             />
           );
         })}
-      {/* </section> */}
     </>
   );
 }
