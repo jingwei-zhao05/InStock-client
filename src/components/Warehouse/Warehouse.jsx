68307ea0d01913b44b027747f8251db96670743c
Thomas Vovk <127060122+Thomasvovk@users.noreply.github.com>
2023-06-08 20:06:52 -0400

    fixes

--- src/components/Warehouse/Warehouse.jsx
+++ src/components/Warehouse/Warehouse.jsx
@@ -32,59 +32,45 @@ export default function Warehouse({
   };
 
   return (
-<<<<<<< HEAD
-    <div className="warehouses__box">
-      <div className="warehouses__text-box">
-        <div className="warehouses__column">
-          <div className="warehouses__info-warehouse">
-            <h3 className="warehouses__mobile-header">Warehouse</h3>
-            <div className="warehouses__chevron">
-              <div className="warehouses__chevron-link">
+    <div className="warehouseList__box">
+      <div className="warehouseList__text-box">
+        <div className="warehouseList__column">
+          <div className="warehouseList__info-warehouse">
+            <h3 className="warehouseList__mobile-header">Warehouse</h3>
+            <div className="warehouseList__chevron">
+              <div className="warehouseList__chevron-link">
                 <Link
                   to={`/warehouses/${id}`}
-                  className="warehouses__link-anchor"
+                  className="warehousesList__link-anchor"
                 >
                   {name}
                 </Link>
                 <img
-                  className="warehouses__chevron-image"
+                  className="warehousesList__chevron-image"
                   src={Chevron}
                   alt="chevron"
                 ></img>
               </div>
             </div>
           </div>
-          <div className="warehouses__info-address">
-            <h3 className="warehouses__mobile-header">Address</h3>
-            <p className="warehouses__text">
+          <div className="warehousesList__info-address">
+            <h3 className="warehousesList__mobile-header">Address</h3>
+            <p className="warehousesList__text">
               {address}, {city}, {country}
             </p>
           </div>
         </div>
-        <div className="warehouses__column-contacts">
-          <div className="warehouses__info-contact">
-            <h3 className="warehouses__mobile-header">Contact Name</h3>
-            <p className="warehouses__contact-text">{contactName}</p>
+        <div className="warehousesList__column-contacts">
+          <div className="warehousesList__info-contact">
+            <h3 className="warehousesList__mobile-header">Contact Name</h3>
+            <p className="warehousesList__contact-text">{contactName}</p>
           </div>
-          <div className="warehouses__info-contact-info">
-            <h3 className="warehouses__mobile-header">Contact Information</h3>
+          <div className="warehousesList__info-contact-info">
+            <h3 className="warehousesList__mobile-header">
+              Contact Information
+            </h3>
             <p>{contactPhone}</p>
             <p>{contactEmail}</p>
-=======
-    <div className="warehouseList__box">
-      <div className="warehouseList__info-warehouse">
-        <h3 class="warehouseList__mobile-header">Warehouse</h3>
-        <div className="warehouseList__chevron">
-          <div className="warehouseList__chevron-link">
-            <Link to={`/warehouses/${id}`} class="warehouseList__link-anchor">
-              {name}
-            </Link>
-            <img
-              class="warehouseList__chevron-image"
-              src={Chevron}
-              alt="chevron"
-            ></img>
->>>>>>> develop
           </div>
         </div>
       </div>
@@ -113,12 +99,9 @@ export default function Warehouse({
             onClick={handleOpenModal}
           />
         </Link>
-<<<<<<< HEAD
-=======
         <Link to={`/warehouses/${id}/edit`} class="warehouseList__link">
           <img src={EditIcon} alt="edit icon" class="warehouseList__icon" />
         </Link>
->>>>>>> develop
       </div>
       <Modal
         isOpen={isModalOpen}
