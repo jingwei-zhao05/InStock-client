960ddc6905fdfea6131cfc834de8d147c29f8a50
Jingwei Zhao <jingwei.zhao0527@gmail.com>
2023-06-11 01:54:33 -0400

    Fix delete modal styling

--- src/modal/InventoryModal/InventoryDetailModal.jsx
+++ src/modal/InventoryModal/InventoryDetailModal.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import close from '../../assets/icons/close-24px.svg';
+import close from "../../assets/icons/close-24px.svg";
 import axios from "axios";
 
-const Modal = ({ isOpen, onClose, itemDetailsName, itemDetailsId, fetchInventories }) => {
+const Modal = ({
+  isOpen,
+  onClose,
+  itemDetailsName,
+  itemDetailsId,
+  fetchInventories,
+}) => {
   const handleDelete = () => {
     axios
       .delete(`http://localhost:8080/inventories/${itemDetailsId}`)
@@ -21,27 +27,30 @@ const Modal = ({ isOpen, onClose, itemDetailsName, itemDetailsId, fetchInventori
   if (!isOpen) {
     return null;
   }
-  console.log(itemDetailsId);
 
   return (
     <div className="modal">
-      <div className="modal-content">
-        <img
-          src={close}
-          alt="X"
-          className="modal__x"
-          onClick={() => {
-            onClose(false);
-          }}
-        />
-        <h2>Delete {itemDetailsName} Inventory Item?</h2>
-        <p className="modal__text">
-          Please confirm that you'd like to delete the {itemDetailsName} from
-          the list of inventories. You won't be able to undo this action.
-        </p>
+      <div className="modal__container">
+        <div>
+          <img
+            src={close}
+            alt="X"
+            className="modal__x"
+            onClick={() => {
+              onClose(false);
+            }}
+          />
+          <h2 className="modal__title">
+            Delete {itemDetailsName} Inventory Item?
+          </h2>
+          <p className="modal__text">
+            Please confirm that you'd like to delete the {itemDetailsName} from
+            the list of inventories. You won't be able to undo this action.
+          </p>
+        </div>
         <div className="modal__button-container">
-          <button className="modal__close" onClick={() => onClose(false)}>
-            Close
+          <button className="modal__cancel" onClick={() => onClose(false)}>
+            Cancel
           </button>
           <button className="modal__delete" onClick={handleDelete}>
             Delete
